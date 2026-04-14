cb12b646c0b4a9d90180e67d4d5a340df15a3f86
MATVIZ4986 <150750606+MATVIZ4986@users.noreply.github.com>
2026-04-13 21:55:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,7 @@ def get_db():
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME"),
         port=int(os.getenv("DB_PORT")),
-        ssl_disabled=True,
-        connection_timeout=10
+        auth_plugin='mysql_native_password'
     )
     return conexion, conexion.cursor(dictionary=True)
 
